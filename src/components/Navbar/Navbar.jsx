07be822451cb3ee6fc8cd1f7826b00235e93278b
Lê Thị Như Ý <157899541+lethinhuy203@users.noreply.github.com>
2024-08-19 12:36:47 +0700

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/portfolio/">
+      <a className={styles.title} href="#portfolio">
         Portfolio
       </a>
       <div className={styles.menu}>
